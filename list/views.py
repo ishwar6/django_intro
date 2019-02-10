c93cf0a941e3524434ca41e9b9d743d067d1f1a9
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-11 03:03:56 +0530

    module update

--- list/views.py
+++ list/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
 
 def home(request):
     context = {
@@ -16,4 +18,30 @@ def contact(request):
         'q' : 'Hello! In case of any query feel free to connect with us'
     }
     return render(request, 'list/home.html', context)
+
+
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course,
+        data=data)
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course,
+        id=pk,
+        owner=request.user)
+        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+    
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                    'formset': formset})
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+        'formset': formset})
 
